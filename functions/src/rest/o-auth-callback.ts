e2a37edb750d0553380bfc8bf328d13fd361e3d9
Filip Lauc <filip.lauc93@gmail.com>
2019-07-07 17:15:26 +0200

    chore: added express as dependency

--- functions/src/rest/o-auth-callback.ts
+++ functions/src/rest/o-auth-callback.ts
@@ -1,10 +1,11 @@
-import * as functions from "firebase-functions";
+import { Request } from "express";
 import * as admin from "firebase-admin";
+import * as functions from "firebase-functions";
 import { constants } from "http2";
 import { functionsOauthClient } from "../utils/o-auth-client";
 
 export const oAuthCallback = functions.https.onRequest(
-  async (req: any, res) => {
+  async (req: Request, res) => {
     res.set("Cache-Control", "private, max-age=0, s-maxage=0");
     const code = req.query.code;
 
